308df053a6d9ad5b0345cb49bb792cf9b47ef9ec
dlutwuwei <dlutwuwei@qq.com>
2016-12-22 21:05:37 +0800

    change youtube block show
    Change-Id: I59c2faf796858b750dd8980d7d25cf0e66dd4dee

--- index.js
+++ index.js
@@ -102,28 +102,30 @@ class MediaEditorExample extends React.Component {
             setTimeout(() => this.focus(), 0);
         });
         if(urlType === 'youtube') {
-            debugger;
-            fetch(`http://localhost:3001/api/video/youtube?url=${encodeURIComponent(urlValue)}`, {
-                method: 'GET',
-                headers: {
-                    credentials: 'same-origin',
-                }
-            }).then((response) => {
-                if (response.status >= 200 && response.status < 300) {
-                    return response
-                } else {
-                    var error = new Error(response.statusText)
-                    error.response = response
-                    throw error
-                }
-            }).then((reponse) => {
-                return reponse.json();
-            }).then(data => {
-                let container = document.createElement('div');
-                container.innerHTML = data.videoInfo;
-                let dataset = container.firstChild.dataset;
-                Entity.mergeData(entityKey, dataset);
+            Entity.mergeData(entityKey, {
+                urlValue
             });
+            // fetch(`http://localhost:3001/api/video/youtube?url=${encodeURIComponent(urlValue)}`, {
+            //     method: 'GET',
+            //     headers: {
+            //         credentials: 'same-origin',
+            //     }
+            // }).then((response) => {
+            //     if (response.status >= 200 && response.status < 300) {
+            //         return response
+            //     } else {
+            //         var error = new Error(response.statusText)
+            //         error.response = response
+            //         throw error
+            //     }
+            // }).then((reponse) => {
+            //     return reponse.json();
+            // }).then(data => {
+            //     let container = document.createElement('div');
+            //     container.innerHTML = data.videoInfo;
+            //     let dataset = container.firstChild.dataset;
+            //     Entity.mergeData(entityKey, dataset);
+            // });
         }
     }
 
--- src/components/youtube-video-block.js
+++ src/components/youtube-video-block.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 
 const YoutbueBlock = (props) => {
-    
+    let id = props.id;
+    if(props.urlValue) {
+        let matchs = /www\.youtube\.com\/watch\?v=(.*)$/.exec(props.urlValue);
+        id = matchs[1] || props.id;
+    }
     return (
         <div>
-            <iframe id={props.id} width={props.width} height={props.height} frameBorder="0" allowFullScreen="1" title="YouTube video player"
-                src={`https://www.youtube.com/embed/${props.id}?enablejsapi=1&origin=${encodeURIComponent(window.location.href)}&widgetid=1`}></iframe>
+            <iframe id={id} width={props.width} height={props.height} frameBorder="0" allowFullScreen="1" title="YouTube video player"
+                src={`https://www.youtube.com/embed/${id}?enablejsapi=1&origin=${encodeURIComponent(window.location.href)}&widgetid=1`}></iframe>
         </div>
     );
 }
